8727ad64e96db0f9a9872f4a969f0bfb20d150c4
alexmyssev <alex.myssev@mail.ru>
2026-07-14 19:30:07 +0300

    Update vector_storage.py

--- vector_storage.py
+++ vector_storage.py
@@ -9,24 +9,24 @@ class VectorStore:
         self.name = "knowledge_base"
         self.client = PersistentClient(path="./chroma_db")
         self.collection = self.client.get_or_create_collection(name = self.name)
-        self.embedding_function = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B",  "gpu",  True)
+        self.embedding_function = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B",  device="cpu", trust_remote_code=True)
 
-    def add_document(self, documents: list[str], metadatas: list[dict[str, Any]]):
+    def add_document(self, documents: list[str], metadatas: list[dict[str, any]]):
         if not documents:
             return
-        embeddings = self.embedding_function(documents).tolist()
+        embeddings = self.embedding_function.encode(documents).tolist()
         ids = [f"doc_{i}" for i in range(len(documents))]
         self.collection.add(documents = documents, metadatas = metadatas, ids = ids, embeddings = embeddings)
 
-    def query (self, query_texts: list[str], n_results: int) -> dict[str, Any]:
-        query_embeddings = self.embedding_function(query_texts).tolist()
+    def retriever (self, query_texts: list[str], n_results: int) -> dict[str, Any]:
+        query_embeddings = self.embedding_function.encode(query_texts).tolist()
         results = self.collection.query(query_embeddings, n_results = n_results)
 
         return {
             "documents": results["documents"][0] if results["documents"] else None,
-            "metadatas": results["metadatas"] if results["metadatas"] else None,
-            "ids": results["ids"] if results["ids"] else None,
-            "distances": results["distances"] if results["distances"] else None
+            "metadatas": results["metadatas"][0] if results["metadatas"] else None,
+            "ids": results["ids"][0] if results["ids"] else None,
+            "distances": results["distances"][0] if results["distances"] else None
         }
 
 
