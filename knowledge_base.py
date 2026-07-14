ac7a04cf3da150de7bdea0dfd699b92c42ece34d
alexmyssev <alex.myssev@mail.ru>
2026-07-14 19:29:16 +0300

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -6,26 +6,23 @@ def chunk_text(text: str):
     chunks = splitter.split_text(text)
     return chunks
 
-
-
-
 def document_chunking(folder: str) -> list[dict[str,any]]:
     all_chunks = []
 
     for filename in os.listdir(folder):
         if filename.endswith(".txt"):
-            with open(os.path.join(folder, filename), 'r') as f:
+            with open(os.path.join(folder, filename), 'r', encoding="utf-8") as f:
                 text = f.read()
                 chunks = chunk_text(text)
 
                 for i, chunk in enumerate(chunks):
                     all_chunks.append({
-                        "text": chunk,
+                        "document": chunk,
                         "metadatas":{
                             "source": filename,
                             "source_path": os.path.join(folder, filename),
                             "chunk_id": i,
-                            "access": allowed_access(filename)
+                            "status": allowed_access(filename)
                         }
                     })
     return all_chunks
