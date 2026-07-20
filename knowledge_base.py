128cd7ca0cff10a468211058aeb8bf5e514ffd3c
alexmyssev <alex.myssev@mail.ru>
2026-07-20 20:25:55 +0300

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -2,7 +2,7 @@ import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter#
 
 def chunk_text(text: str):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=350, chunk_overlap=70)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=70)
     chunks = splitter.split_text(text)
     return chunks
 
