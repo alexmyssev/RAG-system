f16d5ece7c2c22cc27690716b5b2a756ef467190
alexmyssev <alex.myssev@mail.ru>
2026-09-07 03:18:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 AGENT_CONFIG = {
     "temperature": 0.7,
     "max_output_tokens": 2048,
@@ -6,14 +8,19 @@ AGENT_CONFIG = {
 }
 
 PROMPT_FORMAT = """ 
-    <system_instructions> {system_instruction} </system_instructions>
+    <system_instructions_start1> {system_instruction} </system_instructions_end1>
     <untrusted> {context} </untrusted> 
     <user_input> {question} </user_input> 
+    <system_instructions_start2> {system_instruction} </system_instructions_end2>
     """
 
-OLLAMA_MODEL = "llama3.1:8b"
-OLLAMA_HOST = "http://localhost:11434"
-PROMPT_GUARD_MODEL = "protectai/deberta-v3-base-prompt-injection-v2"
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3.1:8b")
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://localhost:11434")
+#EMBEDDING_MODEL = "Qwen/Qwen3-Embedding-0.6B"
+EMBEDDING_MODEL_PATH = "./models/qwen3-embedding"
+#PROMPT_GUARD_MODEL = "protectai/deberta-v3-base-prompt-injection-v2"
+PROMPT_GUARD_MODEL_PATH = "./models/deberta-prompt-injection"
 DEVICE = "cpu"
+
 
 
